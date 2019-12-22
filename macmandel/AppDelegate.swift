cb56360a3fa14fc13c810f7fdd92968d96c94da0
Joe <joe@iachieved.it>
2019-12-22 15:48:17 -0600

    Initial Commit

---
+++ macmandel/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  macmandel
+//
+//  Created by Joseph Bell on 12/22/19.
+//  Copyright © 2019 iAchieved.it LLC. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+  func applicationDidFinishLaunching(_ aNotification: Notification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(_ aNotification: Notification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
---
+++ macmandel/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  macmandel
+//
+//  Created by Joseph Bell on 12/22/19.
+//  Copyright © 2019 iAchieved.it LLC. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: Any? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
